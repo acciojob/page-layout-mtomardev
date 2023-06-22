62e7f7f44ecb52d8ab7949120b0d2097018a4542
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-06-22 14:08:42 +0000

    5thchange

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,19 @@
 
 import React from "react";
 import './../styles/App.css';
-import PageLayout from "./PageLayout";
-
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-        <PageLayout 
-        header = "Welcome to my website"
-        footer = "@ 2023 My Website. All right reserved"
-        >
-          <p>This is the content of my website</p>
-        </PageLayout>
-
+        
+        <Header header = "Welcome to my website"/>
+        <Content content = "This is the content of my website." />
+        <Footer footer = "@ 2023 My Website. All right reserved."/>
+        
 
     </div>
   )
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = ({footer}) =>{
+    return(
+        <div>
+            <h2>{footer}</h2>
+        </div>
+    )
+}
+
+export default Footer
