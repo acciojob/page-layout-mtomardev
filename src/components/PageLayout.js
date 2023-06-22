9ff5db8a1a12402309f59818e9773b81542df80c
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-06-22 13:45:58 +0000

    2nd change

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -3,9 +3,11 @@ import React from "react";
 const PageLayout = ({heading, fContent, children}) =>{
     return(
         <div>
-            <h1>{heading}</h1>
+            <header>{heading}</header>
+            {/* <h1>{heading}</h1> */}
             {children}
-            <h3>{fContent}</h3>
+            <footer>{fContent}</footer>
+            {/* <h3>{fContent}</h3> */}
         </div>
     )
 }
